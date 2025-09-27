25661fd56ad8d136ddb379f30e57fba9172975d9
deFr0ggy <16836050+deFr0ggy@users.noreply.github.com>
2025-09-27 05:15:56 +0300

    vercel update

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react"; // Change this line if you installed @vitejs/plugin-react
 import path from "path";
 
 export default defineConfig(({ mode }) => ({
